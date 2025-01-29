e8a0ac087c4f911072afffef09ed83c42cf462aa
fatima_97 <160069921+fatima317@users.noreply.github.com>
2025-01-30 04:49:24 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Hero from "../components/ui/Hero";
 import About from "../components/ui/About";
 import Experience from "../components/ui/Experience";
 import Skills from "../components/ui/Skills";
 import Cases from "../components/ui/Cases";
 import Contact from "../components/ui/Contact";
+import Main from "../components/ui/Main";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
+      <Main />
       <main className="space-y-12">
         <About />
         <Experience />
---
+++ components/ui/Main.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <section className="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-20">
+    <div className="container mx-auto text-center">
+      <h1 className="text-5xl font-bold mb-4">Fatima Saleem</h1>
+      <p className="text-xl mb-6">Dentist | AI Enthusiast </p>
+      <div className="space-x-4">
+        <a
+          href="/cv.pdf"
+          className="bg-white text-blue-500 px-6 py-3 rounded-lg shadow hover:bg-gray-100"
+          target="_blank" // Opens in a new tab
+          rel="noopener noreferrer" // Security best practice
+        >
+          View CV
+        </a>
+        <a
+          href="#contact"
+          className="bg-purple-700 text-white px-6 py-3 rounded-lg shadow hover:bg-purple-800"
+        >
+          Contact Me
+        </a>
+      </div>
+    </div>
+  </section>
+);
+};
+
+export default Main;
